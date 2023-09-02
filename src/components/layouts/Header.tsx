9f83795453d463c583554f4768d39ddd1b8c61b7
k4Z5h2Y6 <k8bi69x4@gmail.com>
2023-09-02 15:21:35 +0900

    header挙動作成完了

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,31 +1,57 @@
 import Image from "next/image"
+import { Dispatch, SetStateAction } from "react"
 
-export const Header = () => {
+type Props = {
+  isNavOpened: boolean
+  setIsNavOpened: Dispatch<SetStateAction<boolean>>
+}
+
+export const Header = ({ isNavOpened, setIsNavOpened }: Props) => {
   return (
     <>
-      <div className='headerI'>
+      <div
+        className='headerI'
+        onMouseEnter={() => {setIsNavOpened(true)}}
+        onMouseLeave={() => {setIsNavOpened(false)}}
+      >
         <Image
-          src='/Header/soil.png' 
-          alt='image of soil'
+          src='/Header/soil.png'
+          alt='soil'
           width={1920}
-          height={540}
-          //TODOヒロトに相談
-          // layout='responsive'
-          // objectFit='contain'
-          // fill
+          height={400}
+          layout='responsive'
         />
+        <div className='logoO'>
+          <a href="">
+            <Image
+              src='/Header/logoA.png'
+              alt='logo'
+              width={1080}
+              height={1080}
+              layout='responsive'
+            />
+          </a>
+        </div>
       </div>
 
       <style jsx>{`
         .headerI {
           width: 100vw;
-          height: 100%;
           position: fixed;
-          top: 80vh;
+          top: 93vh;
           right: 0;
           left: 0;
           z-index: 10;
           background-color: red;
+        }
+        .logoO {
+          width: 341px;
+          height: 294px;
+          position: fixed;
+          right: 2vw;
+          bottom: 0;
+          z-index: 11;
+          background-color: blue;
         }
       `}</style>
     </>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,23 @@ import { Header } from '@/components/layouts/Header'
 import { NavMenu } from '@/components/layouts/NavMenu'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const [isNavOpened, setIsNavOpened] = useState<boolean>(false);
+
   return (
-    <>
-      <Header/>
+    <>{isNavOpened ? '開いている' : '閉まっている'}
+      <Header
+        isNavOpened={isNavOpened}
+        setIsNavOpened={setIsNavOpened}
+      />
       <div className='mainO'>
         <main>
           <Component {...pageProps} />
         </main>
-        <NavMenu/>
+        <NavMenu />
       </div>
 
       <style jsx>{`
