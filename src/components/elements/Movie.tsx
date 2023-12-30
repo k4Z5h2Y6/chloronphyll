a6d4c6e2dbeeaa6055ee6e54032e817bf348f92f
k4Z5h2Y6 <k8bi69x4@gmail.com>
2023-12-30 13:02:04 +0900

    調整

--- src/components/elements/Movie.tsx
+++ src/components/elements/Movie.tsx
@@ -6,7 +6,8 @@ export const Movie = ({ src }: Props) => {
   return (
     <>
       <div className='MovieO'>
-        <video src={src} autoPlay muted loop controls playsInline />
+        {/* <video src={src} autoPlay muted loop controls playsInline /> */}
+        <video src={src} muted loop controls playsInline />
       </div>
 
       <style jsx>{`
